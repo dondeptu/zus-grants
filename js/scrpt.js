1ebcdc2513a8fa04e6c8cd22ad824fcb8c2cec78
Donata Deptula <dondeptu@gmail.com>
2024-01-27 22:37:02 +0100

    added dropdown menu bar

---
+++ js/scrpt.js
@@ -0,0 +1,12 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+            nav.classList.toggle("navigation__show");
+        })
+    };
+};
+
+showMenu("menu-toggle", "nav-menu");
