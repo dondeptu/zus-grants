57d90b2cdbf082236d5e562e1136b6ca00ecedb4
Donata Deptula <dondeptu@gmail.com>
2024-01-28 00:19:58 +0100

    added handling on closing side menu when clicked outside of it

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,28 +1,38 @@
 {
     const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId);
-    const nav = document.getElementById(navId);
+        const toggle = document.getElementById(toggleId);
+        const nav = document.getElementById(navId);
 
-    if (toggle && nav) {
-        toggle.addEventListener("click", () => {
-            nav.classList.toggle("navigation__show");
-            toggle.classList.toggle("bx-x");
-        })
-    };
-};
-
-showMenu("menu-toggle", "nav-menu");
-
-
-const navLinks = document.querySelectorAll(".js-navLink");
-const linkAction = (e) => {
-
-    navLinks.forEach(n => n.classList.remove("navigation__linkActive"));
-    e.target.classList.add("navigation__linkActive");
-
-}
-navLinks.forEach(n => n.addEventListener("click", linkAction));
+        if (toggle && nav) {
+            toggle.addEventListener("click", () => {
+                nav.classList.toggle("navigation__show");
+                toggle.classList.toggle("bx-x");
+            });
+        };
+        document.onclick = function (e) {
+            if (!toggle.contains(e.target)) {
+                nav.classList.toggle("navigation__show");
+                toggle.classList.toggle("bx-x");
+            }
+        }
+    }
 
 
+    showMenu("menu-toggle", "nav-menu");
+
+
+    const navLinks = document.querySelectorAll(".js-navLink");
+    const linkAction = (e) => {
+        navLinks.forEach(n => n.classList.remove("navigation__linkActive"));
+        e.target.classList.add("navigation__linkActive");
+    }
+    navLinks.forEach(n => n.addEventListener("click", linkAction));
+
+
+    // document.onclick = function(e){
+    //     if (!toggle(e.target)){
+    //         nav.classList.toggle("navigation__show");
+
+    //     }
 
 }
