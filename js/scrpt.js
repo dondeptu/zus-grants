b62b8adc9423acaea926ad041f14b5d05c4e49f3
Donata Deptula <dondeptu@gmail.com>
2024-01-27 23:35:39 +0100

    added handling for selecting active links in the menu

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,4 +1,5 @@
-const showMenu = (toggleId, navId) => {
+{
+    const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId);
     const nav = document.getElementById(navId);
 
@@ -11,3 +12,17 @@ const showMenu = (toggleId, navId) => {
 };
 
 showMenu("menu-toggle", "nav-menu");
+
+
+const navLinks = document.querySelectorAll(".js-navLink");
+const linkAction = (e) => {
+
+    navLinks.forEach(n => n.classList.remove("navigation__linkActive"));
+    e.target.classList.add("navigation__linkActive");
+
+}
+navLinks.forEach(n => n.addEventListener("click", linkAction));
+
+
+
+}
