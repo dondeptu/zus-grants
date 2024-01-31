eb7225c51b47948a3e5fa2f2568464379b7ef26d
Donata Deptula <dondeptu@gmail.com>
2024-02-01 00:17:47 +0100

    added function of closing the side menu window after clicking on the link from the menu

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,4 +1,6 @@
 {
+    const navLinks = document.querySelectorAll(".js-navLink");
+
     const showMenu = (toggleId, navId) => {
         const toggle = document.getElementById(toggleId);
         const nav = document.getElementById(navId);
@@ -11,21 +13,25 @@
         };
         document.onclick = (e) => {
             if (!toggle.contains(e.target) && !nav.contains(e.target)) {
-                nav.classList.toggle("navigation__show");
-                toggle.classList.toggle("bx-x");
+                nav.classList.remove("navigation__show");
+                toggle.classList.remove("bx-x");
             }
-        }
+        };
+        navLinks.forEach(n => n.addEventListener("click", () => {
+            nav.classList.remove("navigation__show");
+            toggle.classList.remove("bx-x");
+        }));
     }
-
 
     showMenu("menu-toggle", "nav-menu");
 
-
-    const navLinks = document.querySelectorAll(".js-navLink");
     const linkAction = (e) => {
         navLinks.forEach(n => n.classList.remove("navigation__linkActive"));
+
         e.target.classList.add("navigation__linkActive");
     }
+
     navLinks.forEach(n => n.addEventListener("click", linkAction));
+
 
 }
